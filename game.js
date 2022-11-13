9140e4d9771f6f966d0d2793f82c405dbb4ea0a6
Daniyal Rai <daniyalrai364@gmail.com>
2022-11-13 02:33:10 +0000

    fix

--- game.js
+++ game.js
@@ -2,83 +2,141 @@ module.exports = {
   createGameState,
   createNewPlayer,
   gameLoop,
-}
+};
 
 const {
   GRIDSIZE,
   BULLET_SPEED,
   PLAYER_SPEED,
   TICKS_PER_UPDATE,
-  UPDATES_PER_SECOND
-} = require("./constants")
+  UPDATES_PER_SECOND,
+  PLAYER_SIZE,
+  FALL_SPEED,
+} = require("./constants");
 
 function createGameState() {
   return {
     players: {},
     bullets: [],
+    platforms: [
+      { startX: 10, startY: GRIDSIZE - 10, endX: GRIDSIZE - 10, endY: GRIDSIZE },
+    ],
     player_speed: PLAYER_SPEED,
     bullet_speed: BULLET_SPEED,
-    tps: TICKS_PER_UPDATE * UPDATES_PER_SECOND,  
-    gridSize: GRIDSIZE
-  }
+    player_size: PLAYER_SIZE,
+    tps: TICKS_PER_UPDATE * UPDATES_PER_SECOND,
+    gridSize: GRIDSIZE,
+  };
 }
 
 function createNewPlayer() {
   return {
-    pos: {x: 10, y: 10},
-    direction: {x: 0, y: 0},
-    health: 100
-  }
+    pos: { x: 10, y: 80 },
+    direction: { x: 0, y: 0 },
+    health: 100,
+    falling: true,
+    jumping: false,
+  };
 }
 
 function gameLoop(gamestate) {
-  let players = gamestate.players
-  let bullets = gamestate.bullets
-  
-  for (playerName in players) {
-    let player = players[playerName]
-    let pos = player.pos
-    let direction = player.direction
+  let players = gamestate.players;
+  let bullets = gamestate.bullets;
+  let platforms = gamestate.platforms;
 
-    pos.x += direction.x * PLAYER_SPEED
-    pos.y += direction.y  * PLAYER_SPEED
+  // Moving
+  for (playerName in players) {
+    let player = players[playerName];
+    let pos = player.pos;
+    let direction = player.direction;
+
+    pos.x += direction.x * PLAYER_SPEED;
+    pos.y += direction.y * PLAYER_SPEED;
   }
 
   bullets.forEach((bullet) => {
-    let pos = bullet.pos
-    let direction = bullet.direction
+    let pos = bullet.pos;
+    let direction = bullet.direction;
 
-    pos.x += direction.x * BULLET_SPEED
-    pos.y += direction.y * BULLET_SPEED
-  })
+    pos.x += direction.x * BULLET_SPEED;
+    pos.y += direction.y * BULLET_SPEED;
+  });
+
+  platforms.forEach((platform) => {
+    let startX = platform.startX;
+    let endX = platform.endX;
+    let startY = platform.startY;
+  });
 
   // Game checks
   for (playerName in players) {
-    let player = players[playerName]
-    let pos = player.pos
+    let player = players[playerName];
+    let pos = player.pos;
+    let direction = player.direction;
+    let falling = player.falling;
 
+    // Bullet hit checks
     bullets.forEach((bullet) => {
-      let bulletPos = bullet.pos
+      let bulletPos = bullet.pos;
       if (
-        bulletPos.x > pos.x && bulletPos.x < pos.x + 5.5 &&
-        bulletPos.y < pos.y && bulletPos.y > pos.y - 5 &&
+        bulletPos.x > pos.x &&
+        bulletPos.x < pos.x + PLAYER_SIZE.x &&
+        bulletPos.y < pos.y &&
+        bulletPos.y > pos.y - PLAYER_SIZE.y &&
         playerName != bullet.player
+      ) {
+        // Headshot check
+        if (
+          bulletPos.y < pos.y - PLAYER_SIZE.y + 1 &&
+          bulletPos.y > pos.y - PLAYER_SIZE.y
         ) {
-        player.health -= 20
-        bullets.splice(bullets.indexOf(bullet), 1)
+          player.health -= 15;
+        }
+
+        player.health -= 15;
+        bullets.splice(bullets.indexOf(bullet), 1);
       }
 
-      if (bullet.pos.x > GRIDSIZE || bullet.pos.x < 0 || bullet.pos.y > GRIDSIZE || bullet.pos.y < 0) {
-        bullets.splice(bullets.indexOf(bullet), 1)
+      if (
+        bullet.pos.x > GRIDSIZE ||
+        bullet.pos.x < 0 ||
+        bullet.pos.y > GRIDSIZE ||
+        bullet.pos.y < 0
+      ) {
+        bullets.splice(bullets.indexOf(bullet), 1);
       }
-    })
+    });
 
-    if (pos.x < 0) {pos.x = GRIDSIZE}
-    else if (pos.x > GRIDSIZE) {pos.x = -1}
+    // Platform checks
+    falling = true;
 
-    if (pos.y < 0) {pos.y = GRIDSIZE}
-    else if (pos.y > GRIDSIZE) {pos.y = -1}
+    platforms.forEach((platform) => {
+      if (
+        falling &&
+        pos.x + PLAYER_SIZE.x / 2 > platform.startX &&
+        pos.x + PLAYER_SIZE.x / 2 < platform.endX &&
+        pos.y > platform.startY &&
+        pos.y < platform.startY + (platform.endY - platform.startY)
+      ) {
+        falling = false;
+      }
+    });
+
+    // Out of bounds checks
+    if (pos.x < 0 + PLAYER_SPEED) {
+      pos.x = 0;
+      direction.x = 0;
+    } else if (pos.x > GRIDSIZE - PLAYER_SPEED - PLAYER_SIZE.x) {
+      pos.x = GRIDSIZE - PLAYER_SIZE.x;
+      direction.x = 0;
+    }
+
+    if (falling) {
+      direction.y = FALL_SPEED;
+    } else {
+      direction.y = 0;
+    }
   }
 
-  return gamestate
+  return gamestate;
 }
--- public/game/script.js
+++ public/game/script.js
@@ -2,27 +2,27 @@ let canvas, context, localGame;
 
 let bgColour = "white";
 
-let bulletColour = "#ffea00"
+let bulletColour = "#ffea00";
 
-let healthColor1 = "#29e823"
-let healthColor2 = "red"
+let healthColor1 = "#29e823";
+let healthColor2 = "red";
 
-let playerIdle = new Image()
-playerIdle.src = "../assets/sprites/playerIdle.png"
+let playerIdle = new Image();
+playerIdle.src = "../assets/sprites/playerIdle.png";
 
-let playerAnims = [new Image(), new Image()]
+let playerAnims = [new Image(), new Image()];
 
-playerAnims[0].src = "../assets/sprites/playerMove1.png"
-playerAnims[1].src = "../assets/sprites/playerMove2.png"
+playerAnims[0].src = "../assets/sprites/playerMove1.png";
+playerAnims[1].src = "../assets/sprites/playerMove2.png";
 
-let animStates = {}
-let framesPerCycle = 10
+let animStates = {};
+let framesPerCycle = 10;
 
-let playerHeight, playerWidth
+let player_size;
 
 let mousePosition = { x: 0, y: 0 };
 
-let time, size;
+let time;
 
 function init() {
   canvas = document.getElementById("canvas");
@@ -30,12 +30,11 @@ function init() {
   canvas.width = canvas.height = 900;
   context.font = "1.2rem Monospace";
 
-  waitingForGame = setInterval(function() {
+  waitingForGame = setInterval(function () {
     if (localGame) {
       // Setting Variables
-      size = canvas.height / localGame.gridSize
-      playerHeight = playerIdle.height * (size * 6.5 / playerIdle.width)
-      playerWidth = size * 5.5
+      size = canvas.height / localGame.gridSize;
+      player_size = localGame.player_size;
 
       window.requestAnimationFrame(() => {
         drawGame(localGame);
@@ -46,7 +45,7 @@ function init() {
 }
 
 function drawGame(game) {
-  resetBoard();
+  resetBoard(game.platforms);
 
   let timePassed = (Date.now() - time) / 1000;
   time = Date.now();
@@ -57,11 +56,11 @@ function drawGame(game) {
 
   for (playerName in game.players) {
     let player = game.players[playerName];
-    let pos = player.pos
+    let pos = player.pos;
     let direction = player.direction;
 
-    pos.x += (direction.x * (game.tps * game.player_speed * timePassed));
-    pos.y += (direction.y * (game.tps * game.player_speed * timePassed));
+    pos.x += direction.x * (game.tps * game.player_speed * timePassed);
+    pos.y += direction.y * (game.tps * game.player_speed * timePassed);
 
     // Taking away direction so it looks like player is moving to next pos
     drawCharacter(player, animStates[playerName]);
@@ -75,87 +74,117 @@ function drawGame(game) {
     pos.y += direction.y * (game.tps * game.bullet_speed * timePassed);
 
     context.fillStyle = bulletColour;
-    context.fillRect(bullet.pos.x * size, bullet.pos.y * size, size * 1.25, size / 2);
-  })
+    context.fillRect(
+      bullet.pos.x * size,
+      bullet.pos.y * size,
+      size / 1.25,
+      size / 1.25
+    );
+  });
 
   window.requestAnimationFrame(() => {
     drawGame(localGame);
   });
 }
 
-function resetBoard() {
+function resetBoard(platforms) {
   context.fillStyle = bgColour;
   context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = "grey";
+  platforms.forEach((platform) => {
+    context.fillRect(
+      platform.startX * size,
+      platform.startY * size,
+      (platform.endX - platform.startX) * size,
+      (platform.endY - platform.startY) * size
+    );
+  });
 }
 
 function drawCharacter(player, animState) {
-  let x = player.pos.x
-  let y = player.pos.y
-  let health = player.health
-  let direction = player.direction
+  let direction = player.direction;
+  let x = player.pos.x;
+  let y = player.pos.y;
+  let health = player.health;
 
   // Player
-  context.save()
+  context.save();
 
   // Flips the player
   if (animState.lastDirection == -1) {
     context.scale(-1, 1);
-    x += playerWidth / size
-    x *= -0.97
+    x += player_size.x;
+    x *= -1;
   }
 
   if (direction.x == 0) {
-    context.drawImage(playerIdle, x * size, (y - 6) * size, playerHeight, playerWidth)
-    
+    context.drawImage(
+      playerIdle,
+      x * size,
+      (y - player_size.y) * size,
+      player_size.x * size,
+      player_size.y * size
+    );
+
     // Reset animation
-    animState.currentAnimFrame = 0
-    animState.currentAnimCycle = 0
+    animState.currentAnimFrame = 0;
+    animState.currentAnimCycle = 0;
   } else {
-    animState.lastDirection = direction.x
+    animState.lastDirection = direction.x;
 
     // Cycles through animations
-    context.drawImage(playerAnims[animState.currentAnimCycle], x * size, (y - 6) * size, playerHeight, playerWidth)
-    animState.currentAnimFrame++
+    context.drawImage(
+      playerAnims[animState.currentAnimCycle],
+      x * size,
+      (y - player_size.y) * size,
+      player_size.x * size,
+      player_size.y * size
+    );
+    
+    animState.currentAnimFrame++;
     if (animState.currentAnimFrame >= framesPerCycle) {
       if (animState.currentAnimCycle == playerAnims.length - 1) {
-        animState.currentAnimCycle = 0
+        animState.currentAnimCycle = 0;
       } else {
-        animState.currentAnimCycle++
+        animState.currentAnimCycle++;
       }
-      animState.currentAnimFrame = 0
+      animState.currentAnimFrame = 0;
     }
   }
-  
+
   // Need to restore canvas state just in case the player was flipped
-  x = player.pos.x
-  context.restore()
+  x = player.pos.x;
+  context.restore();
 
   // Health Bar
-  context.fillStyle = healthColor2
-  context.fillRect((x + 0.5) * size, (y - 7.5) * size, size * 4, size)
+  context.fillStyle = healthColor2;
+  context.fillRect(
+    x * size,
+    (y - player_size.y - 1.25) * size,
+    size * player_size.x,
+    size
+  );
 
   if (health > 0) {
-    context.fillStyle = healthColor1
-    context.fillRect((x + 0.5) * size, (y - 7.5) * size, size * 4 * (health / 100), size)
+    context.fillStyle = healthColor1;
+    context.fillRect(
+      x * size,
+      (y - player_size.y - 1.25) * size,
+      size * player_size.x * (health / 100),
+      size
+    );
   }
 
   // Mouse
   context.beginPath();
-  context.moveTo((x + 5.5) * size, (y - 4) * size)
-  context.lineTo(mousePosition.x, mousePosition.y)
-  context.stroke()
+  context.moveTo(x * size, y * size);
+  context.lineTo(mousePosition.x, mousePosition.y);
+  context.stroke();
 }
 
 function getDirection(key) {
   switch (key.toLowerCase()) {
-    case "arrowup":
-      return (direction = { y: -1 });
-    case "w":
-      return (direction = { y: -1 });
-    case "arrowdown":
-      return (direction = { y: 1 });
-    case "s":
-      return (direction = { y: 1 });
     case "arrowright":
       return (direction = { x: 1 });
     case "d":
@@ -198,16 +227,19 @@ window.addEventListener("blur", () => {
   }
 });
 
-
-
 window.addEventListener("mousemove", (event) => {
-  mousePosition.x = event.clientX - event.target.getBoundingClientRect().left
-  mousePosition.y = event.clientY - event.target.getBoundingClientRect().top
-})
+  mousePosition.x = event.clientX - event.target.getBoundingClientRect().left;
+  mousePosition.y = event.clientY - event.target.getBoundingClientRect().top;
+});
 
 window.addEventListener("mousedown", () => {
-  socket.emit("new-bullet", { x: mousePosition.x / size, y: (mousePosition.y / size)})
-})
+  if (localGame && localGame.players[userName].health > 0) {
+    socket.emit("new-bullet", {
+      x: mousePosition.x / size,
+      y: mousePosition.y / size,
+    });
+  }
+});
 
 socket.on("player-connected", (playerName) => {
   socket.emit("server-message", playerName + " has connected!");
@@ -216,16 +248,16 @@ socket.on("player-connected", (playerName) => {
 });
 
 function updateAnimStates(currentState, players) {
-  players.forEach(playerName => {
+  players.forEach((playerName) => {
     if (!Object.keys(currentState).includes(playerName)) {
       currentState[playerName] = {
         currentAnimCycle: 0,
         currentAnimFrame: 0,
         lastDirection: 1,
-      }
+      };
     }
-  })
-  return currentState
+  });
+  return currentState;
 }
 
 socket.on("new-gamestate", (gamestate) => {
